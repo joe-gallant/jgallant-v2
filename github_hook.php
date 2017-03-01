3af8e43304c8cef39af9c16ffdd7b7005b981dab
Joe Gallant <maryisjoe@Unknown-b8-8d-12-3a-7c-4c.home>
2017-03-01 21:07:44 +0000

    update

--- github_hook.php
+++ github_hook.php
@@ -1,6 +1,2 @@
-<?php
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd public_html/test/ && git reset --hard HEAD && git pull' );
-}
+<?php `git pull`;
 ?>
